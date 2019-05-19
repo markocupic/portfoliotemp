21fb96d978c20ae3cf7c88fbb17ce26bb5c55f0a
Marko Cupic <m.cupic@gmx.ch>
2019-05-19 12:51:16 +0200

    dev

--- src/Resources/contao/dca/tl_portfolio.php
+++ src/Resources/contao/dca/tl_portfolio.php
@@ -55,7 +55,7 @@ $GLOBALS['TL_DCA']['tl_portfolio'] = array
             (
                 'label'      => &$GLOBALS['TL_LANG']['tl_portfolio']['categories'],
                 'href'       => 'table=tl_portfolio_category',
-                'icon'       => 'bundles/markocupicportfoliotemp/icon.png',
+                'icon'       => 'bundles/eufportfolio/icon.png',
                 'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="c"'
             ),
             'all'        => array
